6044575f922d8236c89e311357e39568057cefe0
margOnline <margourey@gmail.com>
2026-04-17 21:45:24 +0100

    Implement start new order button

--- src/components/ProductOrderModal.vue
+++ src/components/ProductOrderModal.vue
@@ -3,6 +3,13 @@ import { icons } from '@/utils/assets'
 import { state } from '@/utils/state'
 import OrderItem from './OrderItem.vue'
 import OrderTotal from './OrderTotal.vue'
+
+function resetState() {
+  state.value.cartProducts = []
+  state.value.cartQty = 0
+  state.value.cartTotal = 0
+  state.value.orderConfirmed = false
+}
 </script>
 
 <template>
@@ -17,10 +24,25 @@ import OrderTotal from './OrderTotal.vue'
     </ul>
 
     <OrderTotal />
+    <button class="btn btn-reverse" @click="resetState()">Start New Order</button>
   </div>
 </template>
 
 <style scoped>
+button {
+  width: 100%;
+}
 .modal-container {
+  background-color: var(--white);
+  padding: 1rem;
+  border-radius: 1rem;
+}
+.btn-reverse {
+  background-color: var(--red);
+  color: var(--white);
+  border: 1px solid var(--red);
+}
+.btn-full-width {
+  width: 100%;
 }
 </style>
