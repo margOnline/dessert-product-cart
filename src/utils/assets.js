00433d1521afc2b53ce5ed746676ff1fdd86d87e
margOnline <margourey@gmail.com>
2026-04-17 18:27:19 +0100

    update images path

--- src/utils/assets.js
+++ src/utils/assets.js
@@ -1,13 +1,13 @@
 export const convertImgFilename = (filename) => {
   return new URL(filename, import.meta.url).href
 }
-const addToCartIcon = convertImgFilename('../assets/images/icon-add-to-cart.svg')
-const decrementIcon = convertImgFilename('../assets/images/icon-decrement-quantity.svg')
-const incrementIcon = convertImgFilename('../assets/images/icon-increment-quantity.svg')
-const removeFromCartIcon = convertImgFilename('../assets/images/icon-remove-item.svg')
-const carbonNeutralIcon = convertImgFilename('../assets/images/icon-carbon-neutral.svg')
-const emptyCartIcon = convertImgFilename('../assets/images/illustration-empty-cart.svg')
-const orderConfirmedIcon = convertImgFilename('../assets/images/icon-order-confirmed.svg')
+const addToCartIcon = convertImgFilename('@/assets/images/icon-add-to-cart.svg')
+const decrementIcon = convertImgFilename('@/assets/images/icon-decrement-quantity.svg')
+const incrementIcon = convertImgFilename('@/assets/images/icon-increment-quantity.svg')
+const removeFromCartIcon = convertImgFilename('@/assets/images/icon-remove-item.svg')
+const carbonNeutralIcon = convertImgFilename('@/assets/images/icon-carbon-neutral.svg')
+const emptyCartIcon = convertImgFilename('@/assets/images/illustration-empty-cart.svg')
+const orderConfirmedIcon = convertImgFilename('@/assets/images/icon-order-confirmed.svg')
 
 export const icons = {
   addToCart: addToCartIcon,
